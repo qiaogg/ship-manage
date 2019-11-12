9cbbf3206dc3941ea80c0f79cb1d558c4b51c3ff
LiXiaRui <40655310+LiXiaRui@users.noreply.github.com>
2019-11-12 20:42:40 +0800

    实现登陆功能

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import axios from 'axios'
 //import VueAxios from 'vue-axios'
 //引入echarts
 import echarts from 'echarts'
+
+import base from './base.js'
+Vue.use(base);
+
+
+
 Vue.use(ElementUI)
 Vue.prototype.$echarts = echarts 
 Vue.config.productionTip = false
@@ -25,27 +31,8 @@ new Vue({
   router,
   components: { App },
   template: '<App/>',
-  mounted(){ 
-    let xmls = '<?xml version="1.0" encoding="utf-8"?> \
-    <soap:Envelope \
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\
-    xmlns:xsd="http://www.w3.org/2001/XMLSchema"\
-    xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"\
-    xmlns:UserWebServiceService="http://webservice.ctbt.com/"\
-    soap:encodingStyle="http://www.w3.org/2001/12/soap-encoding">\
-    <soap:Body>\
-    <UserWebServiceService:CheckUsernameAndPassword><arg0>{"username":"CTBTforV","password":"123456"}</arg0></UserWebServiceService:CheckUsernameAndPassword>\
-    </soap:Body>\
-    </soap:Envelope>'
-    this.$axios
-    .post('/api/CTBT/services/User',xmls)
-    .then(function(response){             
-      console.log(response.data)
-    })
-    .catch(function (error) { // 请求失败处理
-      console.log(error)
-    });
-  }
+
+ 
 })
 /**
  * qQ0CsBemyq1DlGgtJRKK6WVUwQTmnei5,4,1016
